7f19004f8024f6dcf585aced9eb6a508620864ff
David Allen <drallen1@turing.cs.plymouth.edu>
2011-10-31 17:11:24 -0400

    fixed some shit

--- php/studentlistgroup.php
+++ php/studentlistgroup.php
@@ -5,14 +5,16 @@ mysql_select_db("drallen1", $link);
 if (!link){
       die('Error connecting to the database.');
 }
-$query = "SELECT * FROM Students";
+$query = "SELECT s.*, g.* FROM Students s, Groups g WHERE s.group_id=g.group_id ORDER BY s.group_id";
 echo '<FORM METHOD="POST" ACTION="addstudenttogroup".php">';
 $result = mysql_query($query) or die(mysql_error());
 echo "<table border=1px cellspacing=3px>
 <th>Select</th>
 <th>Student ID</th>
 <th>First Name</th>
-<th>Last Name</th>";
+<th>Last Name</th>
+<th>Group</th>";
+
 while($row=mysql_fetch_array($result)){
 echo <<<HTML
 <tr>
@@ -20,27 +22,23 @@ echo <<<HTML
 <td>{$row['STUDENT_ID']}</td>
 <td>{$row['FName']}</td>
 <td>{$row['LName']} </td>
+<td>{$row['GroupName']} </td>
 </tr>
 HTML;
 }
+echo '</table>';
 
-echo "<table border=1px cellspacing=3px>
-<th>GROUP_ID</th>
-<th>Group Name</th>";
+
+//$query = "SELECT GroupName FROM Groups"
 $result = mysql_query($query) or die(mysql_error());
+
 while($row=mysql_fetch_array($result)){
-echo <<<HTML
-<tr>
-<td>{$row['GROUP_ID']}</td>
-<td>{$row['GroupName']}</td>
-</tr>
-HTML;
+//$row['GROUP_ID'] $row['GroupName']
 }
-echo "</table>";
-echo "</table>";
 $query = "SELECT * FROM Groups";
 $result = mysql_query($query) or die(mysql_error());
 echo '<select name="GroupName">';
+
 while ($row = mysql_fetch_array($result)){
 echo "<option>" . $row{'GroupName'} . "</option>";
 }
